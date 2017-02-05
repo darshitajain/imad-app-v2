7d0686fde16996a68960f8c91b83a1dc05da953a
Darshita Jain <darshita1405@gmail.com>
2017-02-05 23:25:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,26 @@ img.onclick = function(){
     var interval = setInterval(moveRight,50);
     
 };
+
+var button = document.getElementById('counter');
+var counter = 0;
+
+ button.onclick = function(){
+    
+    // Make a request to the counter endpoint
+    
+    
+    // Capture the response and store in the variable
+    
+    // render the variable in the correct span
+    counter ++;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+    
+    
+        
+};
+
+
+
