964674c00d71d4e5fd943ecac937b2980174af37
Darshita Jain <darshita1405@gmail.com>
2017-02-05 22:18:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.get('/ui/main.js',function(req,res){
     
 });
 
+var counter = 0;
+app.get('/counter' ,function(req,res){
+    counter ++;
+    res.send(counter.toString());
+});
+
+
+
+
+
 app.get('/:articleName',function(req,res){
    
    var articleName = req.params.articleName;
