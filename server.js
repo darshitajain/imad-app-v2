49bdbfb300cd69304ea3b187e5a7be52650a21b3
Darshita Jain <darshita1405@gmail.com>
2017-03-07 22:28:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config = {
     database:'darshitajain',
     host:'http://db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:'db-darshitajain-54266'
 };
 
 
